aa49591fea4a17615841aafbfed685fb293836ef
algassby <algassby@hotmail.com>
2020-04-03 00:15:09 +0200

    integration du formulaire le projet

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,7 +33,13 @@ class AdController extends AbstractController
      * @return Response
      */
     public function create(){
-        return $this->render('ad/new.html.twig');
+
+        $ad = new Ad();
+        $form = $this->createForm(AdType::class, $ad);
+
+        return $this->render('ad/new.html.twig',[
+            'form'=> $form->createView()
+        ]);
     }
 
     /**
